a976fdebe3a5d86b41e0c8be627a02825620a1d0
Delvin Low <delvinlowzy@gmail.com>
2015-05-09 03:07:21 +0800

    Initial commit

---
+++ app/src/main/java/com/example/android/sunrise/app/MainActivityFragment.java
@@ -0,0 +1,27 @@
+package com.example.android.sunrise.app;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        ArrayList<String> list = new ArrayList<String>();
+        return rootView;
+    }
+}
