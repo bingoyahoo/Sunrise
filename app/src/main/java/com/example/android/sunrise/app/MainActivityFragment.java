10c1c4944064a271ff17af94114f468d610c2234
Delvin Low <delvinlowzy@gmail.com>
2015-05-09 03:51:04 +0800

    Add adapter and bind to ListView

--- app/src/main/java/com/example/android/sunrise/app/MainActivityFragment.java
+++ app/src/main/java/com/example/android/sunrise/app/MainActivityFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -28,10 +30,21 @@ public class MainActivityFragment extends Fragment {
                 "Weds - Cloudy - 72/63",
                 "Thurs- Asteroids - 75/65",
                 "Fri- Heavy Rain - 65/56",
-                "Sat - HELPED TRAPPED IN WEATHERSTATION - 60/51",
+                "Sat - HELP TRAPPED IN WEATHERSTATION - 60/51",
                 "Sun - Sunny - 80/68"
         };
         ArrayList<String> weekForecast = new ArrayList<String>(Arrays.asList(forecastArray));
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                //This fragment's parent Activity
+                getActivity(),
+                //ID of list item layout
+                R.layout.list_item_forecast,
+                //ID of textView to populate
+                R.id.list_item_forecast_textview,
+                //Fake data
+                weekForecast);
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        listView.setAdapter(adapter);
         return rootView;
     }
 }
