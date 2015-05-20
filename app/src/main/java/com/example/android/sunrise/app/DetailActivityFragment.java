b48d4a86b4daed45d2d5980475a7055276b00fe0
Delvin Low <delvinlowzy@gmail.com>
2015-05-20 19:59:49 +0800

    Add projection instead of using getColumnIndex() to be more efficient

--- app/src/main/java/com/example/android/sunrise/app/DetailActivityFragment.java
+++ app/src/main/java/com/example/android/sunrise/app/DetailActivityFragment.java
@@ -37,6 +37,8 @@ public class DetailActivityFragment extends Fragment {
 
         if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
             mForecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
+        }
+        if (mForecastStr != null) {
             TextView textView = (TextView) rootView.findViewById(R.id.detail_text);
             textView.setText(mForecastStr);
         }
